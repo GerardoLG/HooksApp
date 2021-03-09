20b86cbd93b274293b99c9cd348c04eecc37af9a
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-08 22:33:54 -0600

    FormWithCustomHook exercise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SimpleForm } from "./components/useEffect.js/SimpleForm";
+import { FormWithCustomHook } from "./components/useEffect.js/FormWithCustomHook";
+// import { SimpleForm } from "./components/useEffect.js/SimpleForm";
 // import { CounterApp } from "./components/useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/useState/CounterWithCustomHook";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<SimpleForm />, document.getElementById("root"));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
