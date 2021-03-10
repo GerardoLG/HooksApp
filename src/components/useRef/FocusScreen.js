037055ee9d4a44d58cbd788ef7aedfcd8231f2d5
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-10 00:06:03 -0600

    Starting learning useRef Hook

---
+++ src/components/useRef/FocusScreen.js
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import "../useEffect/effect.css";
+
+export const FocusScreen = () => {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <>
+      <h1>Focus Scren</h1>
+      <hr />
+      <input ref={inputRef} className="form-control" placeholder="Nombre" />
+
+      <button className="btn btn-outline-primary mt-3" onClick={handleClick}>
+        Focus
+      </button>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
+import { FocusScreen } from "./components/useRef/FocusScreen";
 // import { FormWithCustomHook } from "./components/useEffect.js/FormWithCustomHook";
 // import { SimpleForm } from "./components/useEffect.js/SimpleForm";
 // import { CounterApp } from "./components/useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/useState/CounterWithCustomHook";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<MultipleCustomHooks />, document.getElementById("root"));
+ReactDOM.render(<FocusScreen />, document.getElementById("root"));
