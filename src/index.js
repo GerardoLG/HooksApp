a59f298d8cff0d7ebbc76f178de179d820dd9a12
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-10 20:32:02 -0600

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
-// import { FocusScreen } from "./components/useRef/FocusScreen";
-import { RealExampleRef } from "./components/useRef/RealExampleRef";
-// import { FormWithCustomHook } from "./components/useEffect.js/FormWithCustomHook";
-// import { SimpleForm } from "./components/useEffect.js/SimpleForm";
-// import { CounterApp } from "./components/useState/CounterApp";
-// import { CounterWithCustomHook } from "./components/useState/CounterWithCustomHook";
+// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { FocusScreen } from "./components/04-useRef/FocusScreen";
+import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+// import { FormWithCustomHook } from "./components/02-useEffect.js/FormWithCustomHook";
+// import { SimpleForm } from "./components/02-useEffect/SimpleForm";
+// import { CounterApp } from "./components/01-useState/CounterApp";
+// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { HookApp } from "./HookApp";
 
 ReactDOM.render(<RealExampleRef />, document.getElementById("root"));
